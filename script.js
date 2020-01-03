669368cd20a6260368bb61b70149850660e88cef
jsmithxyz <jordansmith3813@gmail.com>
2020-01-03 18:09:03 -0500

    added geolocation functionality

--- script.js
+++ script.js
@@ -203,6 +203,39 @@ $(document).ready(function() {
         
       }
 
+      var options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+      };
+      
+      function success(pos) {
+        var crd = pos.coords;
+      
+        console.log('Your current position is:');
+        console.log(`Latitude : ${crd.latitude}`);
+        console.log(`Longitude: ${crd.longitude}`);
+        console.log(`More or less ${crd.accuracy} meters.`);
 
+        currentLocation(crd.latitude, crd.longitude);
+      }
+      
+      function error(err) {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+      }
+      
+      navigator.geolocation.getCurrentPosition(success, error, options);
+
+      function currentLocation(latitude, longitude) {
+        var queryURL = "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid="+ API_KEY;
+
+        $.ajax ({
+            url: queryURL,
+            method: "GET",
+        }).then(function(response){
+           console.log(response);
+          
+        })
+
+    }
 })
-
